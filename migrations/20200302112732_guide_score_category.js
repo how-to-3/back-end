ee36e3401a680dd124ca2f96a8ee055bc8e233c6
Lance Smith <57012297+lance-smith-acc@users.noreply.github.com>
2020-03-02 11:43:17 -0600

    Added categories and score on guide table

---
+++ migrations/20200302112732_guide_score_category.js
@@ -0,0 +1,15 @@
+
+exports.up = async function(knex) {
+  await knex.schema.table('guides', table => {
+    table.string('category', 255)
+        .unique();
+    table.integer('score')
+  });
+};
+
+exports.down = async function(knex) {
+  await knex.schema.table('guides', table => {
+      table.dropColumn('category')
+      table.dropColumn('score')
+  })
+};
