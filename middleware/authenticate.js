f01389ad141d7bf19e16ad740437c588e00586d0
Chris Engel <Chris.Engel@yahoo.com>
2020-03-03 19:45:09 -0500

    cleaned up middleware file

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,8 +1,6 @@
 const jwt = require('jsonwebtoken');
 const { jwtSECRET } = require('../utils/secrets.js');
 
-
-
 const restricted = (req, res, next) => {
   const {authorization} = req.headers;
 
@@ -12,33 +10,12 @@ const restricted = (req, res, next) => {
               res.status(401).json({msg: 'Invalid Credentials'})
           } else {
               req.decodedToken = decodedToken;
-              
               next();
           }
       })
   } else {
-      res.status(400).json({err:'pls provide creds'})
+      res.status(400).json({err:'please provide user credentials'})
   };
 };
-
-
-
-// function restricted(req, res, next){
-//   const { authorization } = req.headers;
-
-//   if(authorization){
-//     jwt.verify(authorization, jwtSECRET, (err, decodedToken) => {
-//       if(err){
-//         res.status(401).json({msg: 'Failed to authenticate user'})
-//       } else {
-//         req.decodedToken = decodedToken;
-//         console.log(decodedToken)
-//         next();
-//       }
-//     })
-//   } else {
-//     res.status(400).json({err:'no credentials found'})
-//   }
-// };
 
 module.exports = restricted
